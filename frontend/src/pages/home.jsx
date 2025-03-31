7cee6723706b557ddb179e6cf85d8e176588d61e
Pratyush <pratyushtiwari446@gmail.com>
2025-03-31 13:26:46 +0530

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import Hero from "../components/hero.jsx";
-import TeamSection from "../components/teamSection.jsx";
+import TeamSection from "../components/TeamSection.jsx";
 // import ContactForm from "../components/ContactForm";
 import Footer from "../components/footer.jsx";
 import IntroSection from "../components/club-intro.jsx";
